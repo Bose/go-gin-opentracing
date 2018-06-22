e9fbc9fd39d8ffcf3cc974748412f9f28a7ec4bd
Jim Lambert <jim_lambert@bose.com>
2018-06-21 22:43:14 -0400

    parameterize operationPrefix

--- middleware.go
+++ middleware.go
@@ -7,15 +7,18 @@ import (
 )
 
 // OpenTracer - middleware that addes opentracing
-func OpenTracer() gin.HandlerFunc {
+func OpenTracer(operationPrefix []byte) gin.HandlerFunc {
+	if operationPrefix == nil {
+		operationPrefix = []byte("api-request-")
+	}
 	return func(c *gin.Context) {
 		// all before request is handled
 		var span opentracing.Span
 		if cspan, ok := c.Get("tracing-context"); ok {
-			span = StartSpanWithParent(cspan.(opentracing.Span).Context(), "api-request-"+c.Request.Method, c.Request.Method, c.Request.URL.Path)
+			span = StartSpanWithParent(cspan.(opentracing.Span).Context(), string(operationPrefix)+c.Request.Method, c.Request.Method, c.Request.URL.Path)
 
 		} else {
-			span = StartSpanWithHeader(&c.Request.Header, "api-request-"+c.Request.Method, c.Request.Method, c.Request.URL.Path)
+			span = StartSpanWithHeader(&c.Request.Header, string(operationPrefix)+c.Request.Method, c.Request.Method, c.Request.URL.Path)
 		}
 		defer span.SetTag(string(ext.HTTPStatusCode), c.Writer.Status()) // this must be before the defer finish to be properly located in the defer stack
 		defer span.Finish()                                              // after all the other defers are completed.. finish the span
