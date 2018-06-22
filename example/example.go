85b73f1892e4cdb4f54ecb1462c0ce92a0766934
Jim Lambert <jim_lambert@bose.com>
2018-06-21 23:01:58 -0400

    working version

--- example/example.go
+++ example/example.go
@@ -1,12 +1,29 @@
 package main
 
 import (
-	"github.com/BoseCorp/go-gin-opentracing"
+	"fmt"
+	"os"
+
+	"github.com/Bose/go-gin-opentracing"
 	"github.com/gin-gonic/gin"
+	opentracing "github.com/opentracing/opentracing-go"
 )
 
 func main() {
-	r := gin.New()
+	r := gin.Default()
+	hostName, err := os.Hostname()
+	if err != nil {
+		hostName = "unknown"
+	}
+
+	tracer, closer, err := ginopentracing.Config.New(fmt.Sprintf("example.go::%s", hostName))
+	if err == nil {
+		fmt.Println("Setting global tracer")
+		defer closer.Close()
+		opentracing.SetGlobalTracer(tracer)
+	} else {
+		fmt.Println("Can't enable tracing: ", err.Error())
+	}
 
 	p := ginopentracing.OpenTracer([]byte("api-request-"))
 	r.Use(p)
